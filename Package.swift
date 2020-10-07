9d6d514f04facc21ecca57b9f3ed2026df9b2f13
Quentin Yann PIDOUX <quentin.pidoux@gmail.com>
2020-10-07 17:37:31 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,27 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "PLogger",
-    platforms: [
-			.macOS(.v10_10), .iOS(.v10), .tvOS(.v9), .watchOS(.v3)
-    ],
+    platforms: [ .macOS(.v10_10), .iOS(.v10), .tvOS(.v9), .watchOS(.v3)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "PLogger",
-            targets: ["PLogger"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "PLogger", targets: ["PLogger"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PLogger",
-            dependencies: []),
-        .testTarget(
-            name: "PLoggerTests",
-            dependencies: ["PLogger"])
+            path: "Source/PLogger")
     ]
 )
